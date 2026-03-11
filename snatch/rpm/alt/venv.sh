3c326a74a950a8094d390bf6362fa39d2a32bc65
George Teys <george.teys@ispras.ru>
2026-03-11 12:30:31 +0300

    venv actions in a separate script

---
+++ snatch/rpm/alt/venv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Virtual environment actions
+
+echo "Activating Python virtual environment"
+cd /opt/snatch/venv/
+
+. env/bin/activate
+python3 $SNATCH_PATH/manage.py makemigrations Snatch
+python3 $SNATCH_PATH/manage.py migrate
+
+# Creating user for CI support
+python3 $SNATCH_PATH/manage.py createsuperuser --username ci_bot --email ci@bot.com --noinput
+echo -e "Please, note that the this token is used to perform CI API requests (it is also saved to /usr/bin/snatch/ci_token.txt)."
+python3 $SNATCH_PATH/manage.py drf_create_token ci_bot | awk '{print $3}' | tee /usr/bin/snatch/ci_token.txt

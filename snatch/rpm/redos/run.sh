f3e217287c99176b1a306c7211e03c5c3a84e323
George Teys <george.teys@ispras.ru>
2026-03-18 13:46:02 +0300

    try to use new features of Alts

--- snatch/rpm/redos/run.sh
+++ snatch/rpm/redos/run.sh
@@ -7,25 +7,53 @@ else
     USER="$(whoami)"
 fi
 
-# Sometimes it's required to stop an existing RabbitMQ process before to avoid issues
-sudo rabbitmqctl shutdown
+# To avoid the multiple password prompts (due to "su" specifics) we are going to create a temporary script 
+#   that will be executed from this script with the required privileges and that will run snatch_start.sh
+echo "#!/bin/bash" > /tmp/post_run.sh
 
-services="rabbitmq-server memcached"
+# Sometimes it's required to stop Snatch before start to avoid some processing issues
+echo "$SNATCH_PATH/snatch_stop.sh" >> /tmp/post_run.sh
+
+pids=()
+
+# Some processes and ports may prevent a correct start of the rabbitmq
+while read -r pid; do
+    pids+=("$pid")
+done < <(pgrep -f rabbit)
+
+while read -r pid; do
+    pids+=("$pid")
+done < <(lsof -t -i :25672)
+
+# To prevent the error: ВАЖНО: пользователь "snatch_user" не прошёл проверку подлинности (по паролю)
+while read -r pid; do
+    pids+=("$pid")
+done < <(pgrep -f celery)
+
+# Kill 'em all
+if [ ${#pids[@]} -gt 0 ]; then
+  echo "kill -9 "${pids[@]}" > /dev/null 2>&1" >> /tmp/post_run.sh
+fi
+
+services="rabbitmq memcached"      # rabbitmq-server 
 for service in $services
 do
-  checkService=$(systemctl status $service)
-  if [[ $checkService != *"running"* ]]; then
-    echo "$service is not started, starting it..."
-    sudo systemctl start $service
-  else
-    echo "$service is started"
-  fi
+  # if systemctl is-active --quiet "$service"; then
+  #   echo "$service is not started"
+    echo "systemctl start $service" >> /tmp/post_run.sh
+  # else
+  #   echo "$service is already started"
+  # fi
 done
 
 running=""
 attempt=1
 
-sudo -u $USER $SNATCH_PATH/snatch_start.sh no-autorun-browser
+echo "sudo -u $USER $SNATCH_PATH/snatch_start.sh" >> /tmp/post_run.sh
+
+chmod +x /tmp/post_run.sh
+su -c "/tmp/post_run.sh"
+
 
 echo "Waiting for SNatch to be started..."
 

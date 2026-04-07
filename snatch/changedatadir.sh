5e9a602898939cdf0f909d4f8eaacc78f923b3ee
George Teys <george.teys@ispras.ru>
2026-04-07 11:53:52 +0300

    Change data dir script

---
+++ snatch/changedatadir.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+settingsFile="/usr/bin/snatch/snatch/settings.py"
+
+
+# Update path function
+updatePath() {
+    local newPath="$1"
+    
+    # Check if the path is valid
+    if [ ! -d "$newPath" ]; then
+        echo "Создание директории: $newPath"
+        mkdir -p "$newPath"
+        if [ $? -ne 0 ]; then
+            echo "Ошибка: Не могу создать каталог $newPath"
+            return 1
+        fi
+    fi
+    
+    # Set permissions
+    chown "$USER":"$USER" "$newPath"
+    chmod 750 "$newPath"
+    
+    # Create backup for the settings file
+    cp "$settingsFile" "$settingsFile.bak"
+    
+    # Update path
+    sed -i "s|^MEDIA_ROOT=.*|MEDIA_ROOT=$newPath|" "$settingsFile"
+    
+    echo "Путь для данных проектов изменен на: $newPath"
+    echo "Конфиг обновлен: $settingsFile"
+    echo "Архивная копия настроек: $settingsFile.bak"
+
+    echo "Перезапуск Snatch ..."
+    /usr/bin/snatch/snatch_stop.sh    
+    /usr/bin/snatch/snatch_start.sh    
+    
+    return 0
+}
+
+
+# Detecting a logged in user
+if [ -n "$SUDO_USER" ]; then
+    USER="$SUDO_USER"
+else
+    USER="$(whoami)"
+fi
+echo "Logged in user: $USER"
+
+currentPath=$(grep "^MEDIA_ROOT=" "$settingsFile" | cut -d'=' -f2)
+echo "Текущий путь для хранения данных проектов: $currentPath"
+read -p "Введите новый путь (или оставьте пустым для отмены): " newPath
+
+if [ -n "$newPath" ]; then
+    updatePath "$newPath"
+else
+    echo "Операция отменена"
+fi

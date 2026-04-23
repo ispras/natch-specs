4c06dc83a3387049d1142ec429e146febce375b8
George Teys <george.teys@ispras.ru>
2026-04-23 15:21:55 +0300

    small impr

--- snatch/rpm/django.sh
+++ snatch/rpm/django.sh
@@ -14,5 +14,5 @@ python3 $SNATCH_PATH/manage.py migrate
 # Creating user for CI support
 python3 $SNATCH_PATH/manage.py createsuperuser --username ci_bot --email ci@bot.com --noinput
 
-echo -e "Внимание! Этот токен может быть использован для CI API запросов (он также сохранён в /usr/bin/snatch/ci_token.txt)."
+echo -e "Внимание! Этот токен может быть использован для CI API запросов (он также сохранён в /usr/bin/snatch/ci_token.txt):"
 python3 $SNATCH_PATH/manage.py drf_create_token ci_bot | awk '{print $3}' | tee /usr/bin/snatch/ci_token.txt

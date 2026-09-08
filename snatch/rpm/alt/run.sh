55f6211d83bc89148c531a79e61eb2fc8f23f785
george.teys <george.teys@ispras.ru>
2026-09-08 18:16:42 +0300

    fix

--- snatch/rpm/alt/run.sh
+++ snatch/rpm/alt/run.sh
@@ -47,7 +47,7 @@ attempt=1
 echo "$SNATCH_PATH/snatch_start.sh" >> /tmp/post_run.sh
 
 chmod +x /tmp/post_run.sh
-su -c "DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS /tmp/post_run.sh" -m
+su - root -c "DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS /tmp/post_run.sh"
 
 echo "Ожидаем запуск SNatch..."
 
